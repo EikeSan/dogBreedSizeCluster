bb9925c59bb298430cc4f4bc68c143b19c986328
EikeSan <eikesantz@hotmail.com>
2018-01-15 16:17:01 -0300

    Add specificity test
    Signed-off-by: EikeSan <eikesantz@hotmail.com>

--- dogBreed.py
+++ dogBreed.py
@@ -1,11 +1,16 @@
+import itertools
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd 
+import random
+import seaborn as sns
+
 from scipy import stats
 from sklearn.cluster import KMeans
 from math import sqrt
-import random
-import seaborn as sns
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 dogDataset = pd.read_csv('AKCBreedInfo.csv',',')
 
@@ -47,6 +52,76 @@ model.fit(data)
 
 
 ### CONFUSION MATRIX
+y = model.labels_
+
+vetor = np.array([data.peso,data.altura])
+#print(vetor)
+class_names = ['pequeno', 'medio', 'grande']
+
+ # Split the data into a training set and a test set
+data_train, data_test, y_train, y_test = train_test_split(data, y, random_state=0)
+
+# Run classifier, using a model that is too regularized (C too low) to see
+# the impact on the results
+classifier = svm.SVC(kernel='linear', C=0.01)
+y_pred = classifier.fit(data_train, y_train).predict(data_test)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    specificity1 = cm[1,1]/(cm[1,0]+cm[1,1])
+    print 'Specificity : ', specificity1
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+plt.savefig('nom-normalizedCM.png')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+plt.savefig('normalizedCM.png')
+
+#plt.show()
+
+
 
 
 #print(model.labels_)
@@ -59,23 +134,6 @@ data = np.array(list(zip(X, Y, model.labels_)))
 data  = pd.DataFrame(data)
 data.columns = ['peso', 'altura','clusters']
 
-#print(model.cluster_centers_)
-#print(model.cluster_centers_[0][0])
-
-#centroids = np.array(list(zip(model.cluster_centers_)))
-#centroid = pd.DataFrame(centroids)
-#centroid.columns = ['x','y']
-
-#print(centroid.x[0])
-
-#plt.legend() 
-# for centroid in model.cluster_centers_:
-
-# plt.scatter(model.cluster_centers_[0][0],model.cluster_centers_[0][1],color=colors[0], s = 60, marker = "o", label= 'Pequeno')
-# plt.scatter(model.cluster_centers_[1][0],model.cluster_centers_[1][1],color=colors[1], s = 60, marker = "o", label= 'Medio') 
-# plt.scatter(model.cluster_centers_[2][0],model.cluster_centers_[2][1],color=colors[2], s = 60, marker = "o", label =  'Grande')
-
-# plt.scatter(data.peso, data.altura, c=colors[model.labels_], s=60)
 
 sns.lmplot('peso', 'altura', data=data, fit_reg=False, hue='clusters',  scatter_kws={"marker": "D", "s": 60},size = 5, aspect = 2)
 plt.title('Cluster Dog Breed Size')
